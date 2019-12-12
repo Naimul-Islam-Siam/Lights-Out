6f05ff6498d2e6b08bc411a7b3c3cae28fa14505
Naimul-Islam-Siam <hallowjokes4@gmail.com>
2019-12-12 22:52:04 +0600

    Updates

---
+++ precache-manifest.82eafda2ad0323f6edef4135e409c9e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82c34cbc78bca665806dc0fa7fba9103",
+    "url": "/Lights-Out/index.html"
+  },
+  {
+    "revision": "265c23e9d5a4d7f8e760",
+    "url": "/Lights-Out/static/css/main.ef57a430.chunk.css"
+  },
+  {
+    "revision": "bd9412fec4d51134cebd",
+    "url": "/Lights-Out/static/js/2.bff05996.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Lights-Out/static/js/2.bff05996.chunk.js.LICENSE"
+  },
+  {
+    "revision": "265c23e9d5a4d7f8e760",
+    "url": "/Lights-Out/static/js/main.a0ea7b7f.chunk.js"
+  },
+  {
+    "revision": "6b3fddeeabd72b3eff3f",
+    "url": "/Lights-Out/static/js/runtime-main.e2617684.js"
+  }
+]);
